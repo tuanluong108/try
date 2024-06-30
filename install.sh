bf69c31e06589e20d82c4974fe9c522bab0b6d3a
Tuan <139671334+tuanluong108@users.noreply.github.com>
2024-06-30 13:12:29 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,8 +2,8 @@
 
 main() {
     echo -e "Downloading Latest Roblox"
-    local version="version-ecb0dc61c2ff4160"
-    curl "http://setup.rbxcdn.com/mac/$version-RobloxPlayer.zip" -o "./RobloxPlayer.zip"
+    [ -f ./RobloxPlayer.zip ] && rm ./RobloxPlayer.zip
+    curl "https://setup.rbxcdn.com/mac/version-ecb0dc61c2ff4160-RobloxPlayer.zip" -o "./RobloxPlayer.zip"
 
     echo -e "Installing Latest Roblox"
     [ -d "/Applications/Roblox.app" ] && rm -rf "/Applications/Roblox.app"
@@ -11,21 +11,29 @@ main() {
     mv ./RobloxPlayer.app /Applications/Roblox.app
     rm ./RobloxPlayer.zip
 
-    echo -e "Downloading try"
-    curl -O "https://raw.githubusercontent.com/tuanluong108/try/raw/main/try.zip"
+    echo -e "Downloading RobloxCheat"
+    rm ./RobloxCheats.dylib
+    curl -LJO "https://github.com/tuanluong108/try/raw/main/RobloxCheats.dylib"
 
-    echo -e "Installing MacSploit"
-    unzip -o -q "./try.zip"
+    echo -e "Downloading libESP"
+    rm ./libESP.dylib
+    curl -LJO "https://github.com/tuanluong108/try/raw/main/libESP.dylib"
+
+    rm ./insert_dylib
+    echo -e "Downloading insert_dylib"
+    curl -LJO "https://github.com/tuanluong108/try/raw/main/insert_dylib"
+
+    chmod +x "./insert_dylib"
 
     echo -e "Patching Roblox"
     mv ./RobloxCheats.dylib "/Applications/Roblox.app/Contents/MacOS/RobloxCheats.dylib"
     mv ./libESP.dylib "/Applications/Roblox.app/Contents/MacOS/libESP.dylib"
-    ./insert_dylib "/Applications/Roblox.app/Contents/MacOS/RobloxCheats.dylib" "/Applications/Roblox.app/Contents/MacOS/RobloxPlayer" --strip-codesig --all-yes
+    ./insert_dylib "/Applications/Roblox.app/Contents/MacOS/libHydrogen.dylib" "/Applications/Roblox.app/Contents/MacOS/RobloxPlayer" --strip-codesig --all-yes
     mv "/Applications/Roblox.app/Contents/MacOS/RobloxPlayer_patched" "/Applications/Roblox.app/Contents/MacOS/RobloxPlayer"
-    rm ./insert_dylib
 
+    chmod +x "/Applications/Roblox.app/Contents/MacOS/RobloxCheats.dylib"
 
-    echo -e "Install Complete! Developed by milo!"
+    echo -e "Install Complete!"
 }
 
 main
